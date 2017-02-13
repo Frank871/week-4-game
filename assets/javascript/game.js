565def32f6a0b02806cd301f50396ef5f6d0a1dc
Franklin Segura <fsegura56734@hotmail.com>
2017-02-13 10:51:02 -0500

    files

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+var A = 0;
+var B = 0;
+var op = null;
+var result = 0;
+
+
+$( "#button-1" ).click(function() {
+
+if (A == 0)
+        {
+         A = this.value;
+         console.log("A triggered: "+ A)
+      }
+ 
+});
